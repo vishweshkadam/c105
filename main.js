cae2ee5013d58bb74c8649df5ceeff9d1d4ffe01
vishweshkadam <75071313+vishweshkadam@users.noreply.github.com>
2021-04-23 13:00:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    });
+}
+
+console.log("ml5 version", ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/L-JMtQ3qN/", modelloaded);
+
+function modelloaded() {
+    console.log("model loaded")
+}
+
+function check() {
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+        document.getElementById("object_Accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
+    
